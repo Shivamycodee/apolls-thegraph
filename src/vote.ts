446a5ad74593c65d2a889fe1dc052b50c39ddb00
Shivamycodee <91057727+Shivamycodee@users.noreply.github.com>
2023-12-10 08:55:32 +0530

    first commit

--- src/vote.ts
+++ src/vote.ts
@@ -1,13 +1,13 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import { Voted as VotedEvent } from "../generated/Vote/Vote"
-import { Voted } from "../generated/schema"
+import { Voted, EntityVoteCollected } from "../generated/schema";
 
 export function handleVoted(event: VotedEvent): void {
   let entity = new Voted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity._from = event.params._from
-  entity._propositionIndex = event.params._propositionIndex
-  entity._proposals = event.params._proposals
+
+  entity.description = event.params.description;
   entity._startVotingTime = event.params._startVotingTime
   entity._endVotingTime = event.params._endVotingTime
 
@@ -15,5 +15,18 @@ export function handleVoted(event: VotedEvent): void {
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
+
+  let userID = entity.description;
+  let entityVoteCollected = EntityVoteCollected.load(userID);
+
+  if(!entityVoteCollected){
+    let entityVoteCollected = new EntityVoteCollected(userID);
+    entityVoteCollected.entity = entity.description;
+    entityVoteCollected.voteCount = BigInt.fromI32(1);
+    entityVoteCollected.save();
+  }else{
+    entityVoteCollected.voteCount = entityVoteCollected.voteCount.plus(BigInt.fromI32(1));
+    entityVoteCollected.save();
+  }
   entity.save()
 }
